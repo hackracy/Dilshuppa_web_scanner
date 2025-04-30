c3f1387e2ff2025145932d2b54c0b93ea9bff190
Dilshuppa <153993902+hackracy@users.noreply.github.com>
2025-04-30 14:47:52 +0530

    Update dilshuppa.sh

--- dilshuppa.sh
+++ dilshuppa.sh
@@ -1,60 +1,34 @@
 #!/bin/bash
 
+echo "ok doing installs pls wait..."
+sudo apt update
+sudo apt install python3 python3-pip git nmap whatweb nikto curl -y
 
-echo " ____ ___ _     ____  _   _ _   _ ____  ____   _    
-|  _ \_ _| |   / ___|| | | | | | |  _ \|  _ \ / \   
-| | | | || |   \___ \| |_| | | | | |_) | |_) / _ \  
-| |_| | || |___ ___) |  _  | |_| |  __/|  __/ ___ \ 
-|____/___|_____|____/|_| |_|\___/|_|   |_| /_/   \_\                                                                                                                            
-                                                     
-"
-echo "  This script is made by Dilshuppa
-"
+echo "installing pythons"
+pip3 install requests
 
-# This script will automatically set up the Dilshuppa Web Recon tool with all dependencies.
+echo "downloading some go stuff..."
 
-echo "Starting the setup process for Dilshuppa_web_recon..."
-
-# Step 1: Install Python dependencies
-echo "Installing Python dependencies..."
-pip install -r requirements.txt
-
-# Step 2: Install Go if it's not installed
 if ! command -v go &> /dev/null; then
-    echo "Go is not installed. Installing Go..."
-    # Detect OS and install Go (you can modify this for specific OS or use a pre-built installer)
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        sudo apt update
-        sudo apt install -y golang-go
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        brew install go
-    else
-        echo "Go installation failed. Please install Go manually."
-        exit 1
-    fi
-else
-    echo "Go is already installed."
+    echo "oh no go not here... getting it"
+    sudo apt install golang -y
 fi
 
-# Step 3: Install WaybackURLs
-echo "Installing WaybackURLs..."
-go install github.com/tomnomnom/waybackurls@latest
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
 
-# Step 4: Install ParamSpider
+go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+
+echo "cloning dirsearch coz why not"
+git clone https://github.com/maurosoria/dirsearch.git tools/dirsearch
+ln -s $(pwd)/tools/dirsearch/dirsearch.py /usr/local/bin/dirsearch
+
+# Installing ParamSpider
 echo "Installing ParamSpider..."
 git clone https://github.com/devanshbatham/ParamSpider.git
 cd ParamSpider
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 cd ..
 
-# Step 5: Install Nuclei
-echo "Installing Nuclei..."
-go install github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
-
-# Step 6: Setup complete
-echo "Setup complete! You can now run the recon tool."
-
-# Optionally, make the Python script executable
-chmod +x Dilshuppa_web_recon.py
-
-echo "To run the tool, use: python3 Dilshuppa_web_recon.py"
+echo "ok all done. run: python3 recon_tool.py"
